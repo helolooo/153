605a686f32fdce534fbb61639296210cdb6513be
helolooo <helloyselimaoliveira@gmail.com>
2024-09-05 10:42:27 -0300

    Add files via upload

---
+++ exercicio27.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class exercicio27{
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+        int dia;
+        System.out.println("Digite odia: ");
+        dia = leia.nextInt();
+        switch (dia) {
+            case 1:
+            System.out.println("Domingo");
+            break;
+
+            case 2:
+            System.out.println("Segunda-Feira");
+            break;
+
+            case 3:
+            System.out.println("Terça-Feira");
+            break;
+
+            case 4:
+            System.out.println("Quarta-Feira");
+            break;
+
+            case 5:
+            System.out.println("Quinta-Feira");
+            break;
+
+            case 6:
+            System.out.println("Sexta-Feira");
+            break;
+
+            case 7:
+            System.out.println("Sábado-Feira");
+            break;
+
+            default:
+            System.out.println("Dia inválido");
+            
+        }
+
+    
+
+    }
+}
